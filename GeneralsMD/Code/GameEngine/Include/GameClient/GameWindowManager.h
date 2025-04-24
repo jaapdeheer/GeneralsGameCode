d8bcd57e762f6456629faa1d7c424d9e0903b5e8
Helmut Buhler <buhler@8gadgetpack.net>
2025-04-24 12:48:57 +0200

    Add comments for DummyGameWindowManager

--- GeneralsMD/Code/GameEngine/Include/GameClient/GameWindowManager.h
+++ GeneralsMD/Code/GameEngine/Include/GameClient/GameWindowManager.h
@@ -379,6 +379,9 @@ extern WindowMsgHandledType PassMessagesToParentSystem( GameWindow *window,
 																															 WindowMsgData mData1, 
 																															 WindowMsgData mData2 );
 
+
+// TheSuperHackers @feature helmutbuhler 24/04/2025
+// GameWindow that does nothing. Used for Headless Mode.
 class DummyGameWindow : public GameWindow
 {
 	MEMORY_POOL_GLUE_WITH_USERLOOKUP_CREATE(DummyGameWindow, "DummyGameWindow")
@@ -387,6 +390,8 @@ public:
 	virtual void winDrawBorder() { }
 };
 
+// TheSuperHackers @feature helmutbuhler 24/04/2025
+// GameWindowManager that does nothing. Used for Headless Mode.
 class DummyGameWindowManager : public GameWindowManager
 {
 public:
